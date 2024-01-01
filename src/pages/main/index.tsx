9b70664bce0c66e96ba848464cd4c36b948a1292
zizonyoungjun <naya700@naver.com>
2024-01-01 17:13:13 +0900

    Design: 지도 상단 공백 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import MapComponent from './map';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import SearchBar from '../search/serchBar';
 
 function Main() {
   return (
     <div>
       <Header/>
+      <SearchBar/>
       <MapComponent />
       <Footer/>
     </div>
