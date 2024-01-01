f961fa41832ff1214e624b04b710fc5f46e8005d
zizonyoungjun <naya700@naver.com>
2024-01-01 16:37:05 +0900

    Design: icon 정리

--- src/assets/images/icons/index.ts
+++ src/assets/images/icons/index.ts
@@ -13,6 +13,24 @@ export { ReactComponent as IcSoju } from './IcSoju.svg';
 export { ReactComponent as IcStar } from './IcStar.svg';
 export { ReactComponent as IcWestern } from './IcWestern.svg';
 export { ReactComponent as IcWorld } from './IcWorld.svg';
+export { ReactComponent as IcSetting } from './IcSetting.svg';
+
+
+export { default as PicBoo } from './IcBoo.svg';
+export { default as PicBoonsik } from './IcBoonsik.svg';
+export { default as PicCafe } from './IcCafe.svg';
+export { default as PicChinese } from './IcChinese.svg';
+export { default as PicFastFood } from './IcFastFood.svg';
+export { default as PicJapanese } from './IcJapanese.svg';
+export { default as PicKorean } from './IcKorean.svg';
+export { default as PicMap } from './IcMap.svg';
+export { default as PicMeat } from './IcMeat.svg';
+export { default as PicMypage } from './IcMypage.svg';
+export { default as PicNotice } from './IcNotice.svg';
+export { default as PicSoju } from './IcSoju.svg';
+export { default as PicStar } from './IcStar.svg';
+export { default as PicWestern } from './IcWestern.svg';
+export { default as PicWorld } from './IcWorld.svg';
 
 //export { default as IconSea } from './IcSea.png';
 
