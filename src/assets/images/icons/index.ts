f4ebc3c422f2cdde7582e53684737d7bfafc8fc2
zizonyoungjun <naya700@naver.com>
2024-01-08 00:07:08 +0900

    Design: icon 추가 및 변경

--- src/assets/images/icons/index.ts
+++ src/assets/images/icons/index.ts
@@ -10,11 +10,12 @@ export { ReactComponent as IcMeat } from './IcMeat.svg';
 export { ReactComponent as IcMypage } from './IcMypage.svg';
 export { ReactComponent as IcNotice } from './IcNotice.svg';
 export { ReactComponent as IcSoju } from './IcSoju.svg';
-export { ReactComponent as IcStar } from './IcStar.svg';
 export { ReactComponent as IcWestern } from './IcWestern.svg';
 export { ReactComponent as IcWorld } from './IcWorld.svg';
 export { ReactComponent as IcSetting } from './IcSetting.svg';
 export { ReactComponent as IcSearch } from './IcSearch.svg';
+export { ReactComponent as IcTrash } from './IcTrash.svg';
+export { ReactComponent as IcEdit } from './IcEdit.svg';
 
 
 export { default as PicBoo } from './IcBoo.svg';
@@ -29,9 +30,30 @@ export { default as PicMeat } from './IcMeat.svg';
 export { default as PicMypage } from './IcMypage.svg';
 export { default as PicNotice } from './IcNotice.svg';
 export { default as PicSoju } from './IcSoju.svg';
-export { default as PicStar } from './IcStar.svg';
 export { default as PicWestern } from './IcWestern.svg';
 export { default as PicWorld } from './IcWorld.svg';
 
 //export { default as IconSea } from './IcSea.png';
 
+export { default as LeftArrow } from './icon-leftArrow.svg';
+export { default as RightArrow } from './icon-rightArrow.svg';
+export { default as SwiperLeft } from './icon-swiperLeft.svg';
+export { default as SwiperRight } from './icon-swiperRight.svg';
+export { default as PopModal } from './icon-PopModal.svg';
+
+export { default as ModalRightOn } from './icon-modalRightOn.svg';
+export { default as ModalLeftOff } from './icon-modalLeftOff.svg';
+export { default as ModalRightOff } from './icon-modalRightOff.svg';
+export { default as ModalLeftOn } from './icon-modalLeftOn.svg';
+
+export { default as TransLeftOff } from './icon-transparentLeftOff.svg';
+export { default as TransLeftOn } from './icon-transparentLeftOn.svg';
+export { default as TransRightOff } from './icon-transparentRightOff.svg';
+export { default as TransRightOn } from './icon-transparentRightOn.svg';
+
+export { default as Chevron_Left } from './Chevron_Left.svg';
+export { default as Chevron_Right } from './Chevron_Right.svg';
+
+export { default as Close } from './icon-close.svg';
+export { ReactComponent as IcHUFSMEALS } from './IcHUFSMEALS.svg';
+export { ReactComponent as IcGoogleLogin } from './IcGoogleLogin.svg';
