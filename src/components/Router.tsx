705b10cd290085ebb694271ba58a628398c3db54
zizonyoungjun <naya700@naver.com>
2024-01-10 12:19:02 +0900

    Feat: 라우터 추가

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,6 +4,8 @@ import Main from "../pages/main/index";
 import MyPage from "../pages/mypage";
 import Notice from "../pages/notice";
 import Login from "../pages/login";
+import Shop from "../pages/shop";
+import SearchResult from "../pages/search/searchResult";
 
 const Router = createBrowserRouter([
   {
@@ -21,6 +23,14 @@ const Router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/shop",
+    element: <Shop />,
+  },
+  {
+    path: "/searchResult",
+    element: <SearchResult />,
   },
 ]);
 
