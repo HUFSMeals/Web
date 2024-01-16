017d2d4da6abbf5b9d29bc28543b4991f63e868e
zizonyoungjun <naya700@naver.com>
2024-01-17 01:14:12 +0900

    feat: 마이 리뷰 수정 삭제 연결

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { IcBoo,IcEdit,IcTrash } from "../../assets/images/icons";
+import { IcBoo,IcEdit,IcTrash,IcKorean } from "../../assets/images/icons";
 //import { Link } from "react-router-dom";
 import Carousel from "../../components/carousel/Carousel";
 import { RateStars } from "../review/rate";
+import { useNavigate } from 'react-router-dom';
+import Modal from "../../components/Modal";
 
   
 const MyPageLogContainer = styled.div`
@@ -95,6 +97,7 @@ const ReviewBody = styled.p`
   padding: 16px;
   margin: 0;
   color: #333;
+  font-size: 16px;
 `;
 
 const myReview ={
@@ -135,11 +138,70 @@ const myReview ={
 					"review_image" : "https://source.unsplash.com/random/?Cryptocurrency&4"
 				},
 			]
+		},
+    {
+			"id" : 3, // 리뷰 id
+			"nickname" : "김멋사", // 리뷰 작성자 닉네임
+			"title" : "어쩌고",
+			"body" : "123", // 리뷰 본문
+			"created_at" : "2024-01-03",
+			"score" : 4.5, // 평점
+			"image" : [
+				{
+					"id" : 1, // 이미지 id
+					"review_image" : "https://source.unsplash.com/random/?Cryptocurrency&2"
+				},
+				{
+					"id" : 2,
+					"review_image" : "https://source.unsplash.com/random/?Cryptocurrency&1"
+				},
+			]
+		},
+    {
+			"id" : 4, // 리뷰 id
+			"nickname" : "김멋사", // 리뷰 작성자 닉네임
+			"title" : "저쩌고",
+			"body" : "456", // 리뷰 본문
+			"created_at" : "2024-01-01",
+			"score" : 4, // 평점
+			"image" : [
+				{
+					"id" : 1, // 이미지 id
+					"review_image" : "https://source.unsplash.com/random/?Cryptocurrency&3"
+				},
+				{
+					"id" : 2,
+					"review_image" : "https://source.unsplash.com/random/?Cryptocurrency&4"
+				},
+			]
 		}
 	]
 }
 
 const MyPage = () => {
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedReviewId, setSelectedReviewId] = useState<number | null>(null);
+
+  const handleEditClick = () => {
+    navigate('/createReview');
+  };
+
+  const handleTrashClick = (reviewId : number) => {
+    setSelectedReviewId(reviewId);
+    setIsModalOpen(true);
+  };
+
+  const handleDeleteConfirm = () => {
+    // Perform deletion here, maybe call an API
+    console.log(`Deleting review with ID: ${selectedReviewId}`);
+    setIsModalOpen(false);
+  };
+  const handleDeleteCancel = () => {
+    setIsModalOpen(false);
+    };
+    
+    
 
   return (
     <MyPageLogContainer>
@@ -153,7 +215,7 @@ const MyPage = () => {
         {myReview.data.map((review) => (
           <ReviewCard key={review.id}>
             <ReviewHeader>
-              <IcBoo style={{ width: "40px", height: "40px" }}/> 
+              <IcKorean style={{ width: "40px", height: "40px" }}/> 
               {/* 가게 사진으로 변경 예정 */}
               <ReviewInfo>
                 <ReviewTitle>{review.title}</ReviewTitle>
@@ -163,8 +225,8 @@ const MyPage = () => {
                 </ReviewRateDate>
               </ReviewInfo>
               <ReviewActions>
-                <IcEdit />
-                <IcTrash />
+                <IcEdit onClick={handleEditClick} />
+                <IcTrash onClick={() => handleTrashClick(review.id)} />
               </ReviewActions>
             </ReviewHeader>
             <ReviewBody>{review.body}</ReviewBody>
@@ -173,6 +235,16 @@ const MyPage = () => {
         ))}
       </ReviewContainer>
       <Footer />
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={handleDeleteCancel}
+          title="리뷰 삭제하기"
+          text1="리뷰를 삭제 하시겠습니까?"
+          action="삭제"
+          onAction={handleDeleteConfirm}
+        />
+      )}
     </MyPageLogContainer>
   );
 };
